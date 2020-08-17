b0c4a1866bc8f8631301010501e0b009c093ded0
czy <1009281636@qq.com>
2020-08-17 23:18:29 +0800

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+import { createElement, render, Component } from './toy-react.js';
+
+class MyComponent extends Component{
+    render(){
+    return <div><h1>my component</h1>{this.children}</div>; 
+    }
+}
+
+render(<MyComponent id="a" class="c"><div>abc</div><div /><div /></MyComponent>, document.body);
